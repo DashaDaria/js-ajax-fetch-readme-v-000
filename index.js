fbe30eea31cac69501c3c70b11de28fdcab35c4d
Dasha <dashadariallc@gmail.com>
2018-01-24 20:26:04 +0800

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const token = '9e045bc9b201b3ffbe1ab2c0eb5dbe1783221f1b'
-fetch('https://api.github.com/user/repos', {
-  headers:{
-    Authorization: `token ${token}`
-  }
-}).then(res => res.json()).then(json => console.log(json));
